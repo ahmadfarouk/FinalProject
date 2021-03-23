2635b10de4a007b722f4249ba269ddb2d798fd5d
Loic Tiemani <loictiems@github.com>
2021-03-23 12:57:35 -0400

    Update stock.js

--- stock.js
+++ stock.js
@@ -29,3 +29,22 @@ function getMonthlyData() {
       var lowPrices =unpack(data.dataset.data,3)
       var closingPrices= unpack(data.dataset.data, 4)
       var volume = unpack(data.dataset.data,5)
+        
+    buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, volume);
+});
+}
+
+function buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, volume) {
+var table = d3.select("#summary-table");
+var tbody = table.select("tbody");
+var trow;
+for (var i = 0; i < 12; i++) {
+  trow = tbody.append("tr");
+  trow.append("td").text(dates[i]);
+  trow.append("td").text(openPrices[i]);
+  trow.append("td").text(highPrices[i]);
+  trow.append("td").text(lowPrices[i]);
+  trow.append("td").text(closingPrices[i]);
+  trow.append("td").text(volume[i]);
+}
+}
