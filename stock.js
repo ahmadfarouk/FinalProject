1f528351a4840c69b40e7085822ff13743d480c7
Loic Tiemani <loictiems@github.com>
2021-03-23 12:56:58 -0400

    Update stock.js

---
+++ stock.js
@@ -0,0 +1,19 @@
+var apiKey = "eys9Qowq7xsT5zdCr6-m";
+
+/**
+ * Helper function to select stock data
+ * Returns an array of values
+ * @param {array} rows
+ * @param {integer} index
+ * index 0 - Date
+ * index 1 - Open
+ * index 2 - High
+ * index 3 - Low
+ * index 4 - Close
+ * index 5 - Volume
+ */
+function unpack(rows, index) {
+  return rows.map(function(row) {
+    return row[index];
+  });
+}
