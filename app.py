7d7d7ea83527245216f5809d0bb6fe946d7088b5
Ahmad Abo Alafa <ahmad_farouk@hotmail.com>
2021-03-23 00:28:45 -0400

    build routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, redirect
 from fbprophet import Prophet
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -12,7 +12,7 @@ def save_dataset(symbol):
 
     ts = TimeSeries(key=api_key, output_format='pandas')
     data, meta_data = ts.get_daily(symbol, outputsize='full')
-    filename = f'./DataOutput/{symbol}_daily.csv'
+    filename = f'./templates/DataOutput/{symbol}_daily.csv'
     data.to_csv(filename)
     return data, filename
 
@@ -41,51 +41,63 @@ def csv_to_dataset_fbprophet(csv_path):
     
     return data,data_openclose, data_volume, data_high, data_low
 
-def csv_to_dataset_volume(csv_path):
-    data = pd.read_csv(csv_path)
+def fbprophet_predict (data,data_openclose, data_volume, data_high, data_low):
+    model_openclose = Prophet(daily_seasonality=True)
+    model_volume = Prophet(daily_seasonality=True)
+    model_high = Prophet(daily_seasonality=True)
+    model_low = Prophet(daily_seasonality=True)
 
-    return data
+    model_openclose.fit(data_openclose)
+    model_volume.fit(data_volume)
+    model_high.fit(data_high)
+    model_low.fit(data_low)
 
-save_dataset(symbol)
+    future_openclose_df = model_openclose.make_future_dataframe(periods=90)
+    future_volume_df = model_volume.make_future_dataframe(periods=90)
+    future_high_df = model_high.make_future_dataframe(periods=90)
+    future_low_df = model_low.make_future_dataframe(periods=90)
 
-data,data_openclose, data_volume, data_high, data_low = csv_to_dataset_fbprophet(f'./DataOutput/{symbol}_daily.csv')
+    predictions_openclose = model_openclose.predict(future_openclose_df)
+    predictions_volume = model_volume.predict(future_volume_df)
+    predictions_high = model_high.predict(future_high_df)
+    predictions_low = model_low.predict(future_low_df)
 
-model_openclose = Prophet(daily_seasonality=True)
-model_volume = Prophet(daily_seasonality=True)
-model_high = Prophet(daily_seasonality=True)
-model_low = Prophet(daily_seasonality=True)
+    model_openclose.plot(predictions_openclose)
+    plt.ylabel("Open-Close Price ", rotation='vertical', weight='bold')
+    plt.xlabel("Year",weight='bold')
+    plt.savefig(f"static/images/Open_Close.png")
 
-model_openclose.fit(data_openclose)
-model_volume.fit(data_volume)
-model_high.fit(data_high)
-model_low.fit(data_low)
+    model_volume.plot(predictions_volume)
+    plt.ylabel("Volume ", rotation='vertical', weight='bold')
+    plt.xlabel("Year",weight='bold')
+    plt.savefig(f"static/images/Volume.png")
 
-future_openclose_df = model_openclose.make_future_dataframe(periods=90)
-future_volume_df = model_volume.make_future_dataframe(periods=90)
-future_high_df = model_high.make_future_dataframe(periods=90)
-future_low_df = model_low.make_future_dataframe(periods=90)
+    model_high.plot(predictions_high)
+    plt.ylabel("High Price ", rotation='vertical', weight='bold')
+    plt.xlabel("Year",weight='bold')
+    plt.savefig(f"static/images/HighPrice.png")
 
-predictions_openclose = model_openclose.predict(future_openclose_df)
-predictions_volume = model_volume.predict(future_volume_df)
-predictions_high = model_high.predict(future_high_df)
-predictions_low = model_low.predict(future_low_df)
+    model_low.plot(predictions_low)
+    plt.ylabel("Low Price ", rotation='vertical', weight='bold')
+    plt.xlabel("Year",weight='bold')
+    plt.savefig(f"static/images/LowPrice.png")
 
-ax =model_openclose.plot(predictions_openclose)
-plt.ylabel("Open-Close Price ", rotation='vertical', weight='bold')
-plt.xlabel("Year",weight='bold')
-plt.savefig(f"./DataOutput/{symbol}_Open_Close.png")
+def export_csv_to_html(csv_file):
+    return csv_file
 
-model_volume.plot(predictions_volume)
-plt.ylabel("Volume ", rotation='vertical', weight='bold')
-plt.xlabel("Year",weight='bold')
-plt.savefig(f"./DataOutput/{symbol}_Volume.png")
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-model_high.plot(predictions_high)
-plt.ylabel("High Price ", rotation='vertical', weight='bold')
-plt.xlabel("Year",weight='bold')
-plt.savefig(f"./DataOutput/{symbol}_HighPrice.png")
+@app.route('/api/PredictStock', methods=['POST'])
+def PredictStock():
+    symbol = request.form['symbol']
+    save_dataset(symbol)
+    data,data_openclose, data_volume, data_high, data_low = csv_to_dataset_fbprophet(f'./templates/DataOutput/{symbol}_daily.csv')
+    fbprophet_predict(data,data_openclose, data_volume, data_high, data_low)
+    export_csv_to_html(f'./templates/DataOutput/{symbol}_daily.csv')
+    return redirect("/")
 
-model_low.plot(predictions_low)
-plt.ylabel("Low Price ", rotation='vertical', weight='bold')
-plt.xlabel("Year",weight='bold')
-plt.savefig(f"./DataOutput/{symbol}_LowPrice.png")
+if __name__ == "__main__":
+    app.run(debug=True)
